3a64966520aed41ebd11b640e6a06f535c4ff958
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-03-06 11:00:58 +0100

    Merge branch dev Add table creation for gym_user, user_training, session and training

--- sql/db_init.sql
+++ sql/db_init.sql
@@ -30,3 +30,37 @@ CREATE TABLE age_category(
   max_age smallint NOT NULL,
   category_name varchar(20) not null
 );
+
+-- need gender, age_category and weight_category
+CREATE TABLE gym_user (
+  user_login VARCHAR(15),
+  nickname VARCHAR(20),
+  age SMALLINT NOT NULL,
+  gender INT REFERENCES gender(gender_id),
+  weight SMALLINT NOT NULL,
+  age_category INT REFERENCES age_category(age_cateory_id),
+  weight_category INT REFERENCES weight_category(weight_category_id),
+  PRIMARY KEY(user_login)
+);
+
+-- need gym_user and training
+CREATE TABLE user_training (
+  user_login VARCHAR(15) REFERENCES gym_user(user_login),
+  training_id INT REFERENCES training(training_id)
+);
+
+-- need gym_user
+CREATE TABLE session (
+  user_login VARCHAR(15) REFERENCES gym_user(user_login),
+  session_id VARCHAR(100),
+  login_data TIMESTAMP
+);
+
+-- need exercise and set_scheme
+CREATE TABLE training (
+  training_id INT,
+  exercise_id INT REFERENCES exercise(exercise_id),
+  set_id INT REFERENCES set_scheme(set_id),
+  training_date TIMESTAMP,
+  PRIMARY KEY(trainig_id)
+);
